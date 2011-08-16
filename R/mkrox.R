b16c2618377d277578bb47d6ab7f5b862564bfd8
dichika <ichikawa@hcc-jp.com>
2011-08-17 03:44:03 +0900

    modify roxygen document

--- R/mkrox.R
+++ R/mkrox.R
@@ -1,48 +1,28 @@
-#' %% ~~function to do ... ~~
+#' make roxygen-style file from function code
 #' 
-#' %% ~~ A concise (1-5 lines) description of what the function does. ~~
+#' make roxygen-style file from function code.
 #' 
 #' %% ~~ If necessary, more details than the description above ~~
 #' 
-#' @param func %% ~~Describe \code{func} here~~
-#' @param pkgdir %% ~~Describe \code{pkgdir} here~~
-#' @return %% ~Describe the value returned %% If it is a LIST, use %%
-#'   \item{comp1 }{Description of 'comp1'} %% \item{comp2 }{Description of
-#'   'comp2'} %% ...
+#' @param func character
+#' @param pkgdir nedd a full path like "paste(sep="/", getwd(), ''/batade/"
+#' @return NULL ( a roxygen-style R source code)
 #' @note %% ~~further notes~~
-#' @author %% ~~who you are~~
+#' @author Ichikawa Daisuke
 #' @seealso %% ~~objects to See Also as \code{\link{help}}, ~~~
 #' @references %% ~put references to the literature/web site here ~
 #' @keywords ~kwd1 ~kwd2
 #' @examples
 #' 
-#' ##---- Should be DIRECTLY executable !! ----
-#' ##-- ==>  Define data, use random,
-#' ##--	or do  help(data=index)  for the standard data sets.
-#' 
-#' ## The function is currently defined as
-#' function(func, pkgdir){
-#'     if(!is.character(func)){
-#'     	stop("func must be character")
-#'     }
-#'     require(stringr)
-#'     require(Rd2roxygen)
-#'     mandir <- str_c(pkgdir, "/man/")
-#'     codedir <- str_c(pkgdir, "/R/")
-#'     
-#'     #output Rd file
-#'     Rdname <- str_c(mandir, func, ".Rd")
-#'     obj <- eval(parse(text=func))
-#'     prompt(obj, Rdname)
-#' 
-#'     #output roxygen code
-#'     codename <- str_c(codedir, func, ".R")
-#'     parse_and_save(path=Rdname, file=codename)
-#' 
-#'     #join roxygen and function code
-#'     dump(func, file=codename, append=TRUE) 
-#'     }
-#' 
+
+#' plot_num <- function(n){
+#'	num <- runif(n)
+#'	plot(num)
+#'	}
+#'dir.create("sample")
+#'dir.create("sample/R")
+#'dir.create("sample/man")
+#'mkrox("plot_num",pkgdir=str_c(getwd(), "/sample"))
 
 mkrox <-
 function(func, pkgdir){
